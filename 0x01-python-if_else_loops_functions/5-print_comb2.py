a880552d7b79505532b1451dde36285863bc8a17
laurendobratz00 <laurendobratz@icloud.com>
2020-12-08 21:16:08 +0000

    changed print statements

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -2,6 +2,7 @@
 l = 00
 u = 100
 for num in range(l, u):
-    print("{0:02d},".format(num), end=" ")
-if num == 99:
-    print("{}".format(num))
+    if num != 99:
+        print("{0:02d},".format(num), end=" ")
+    else:
+        print("{}".format(num))
