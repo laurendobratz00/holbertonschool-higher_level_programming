58a7283b445c547a86ce88eb505e2b6062195301
laurendobratz00 <laurendobratz@icloud.com>
2020-12-09 01:57:16 +0000

    change format

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def print_last_digit(number):
-    return number % 10
-    num = int(input())
-    last_digit = print_last_digit(num)
-    print("{}".format(last_digit))
+    number = str(number)
+    return int(number[len(number) - 1])
