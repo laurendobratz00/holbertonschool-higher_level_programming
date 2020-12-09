b912cd859db575379aa666a09a01642ecf27c506
laurendobratz00 <laurendobratz@icloud.com>
2020-12-09 02:34:41 +0000

    changed return statement

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def print_last_digit(number):
     if number < 0:
-        number = number * -1
-    return number % 10
-    print("{:d}".format(number), end="")
+        number *= -1
+    num = number % 10
+    print("{:d}".format(num), end="")
+    return num
