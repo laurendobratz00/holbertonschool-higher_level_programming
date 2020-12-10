21c0238f41f8e4f29dd9d9428eaead2a0b8deace
laurendobratz00 <laurendobratz@icloud.com>
2020-12-10 00:41:44 +0000

    account for negative n

--- 0x01-python-if_else_loops_functions/101-remove_char_at.py
+++ 0x01-python-if_else_loops_functions/101-remove_char_at.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 def remove_char_at(str, n):
-    if len(str) > n:
-        str = str[0 : n : ] + str[n + 1 : :]
-    return str
+    if n >= 0:
+        return str[0 : n : ] + str[n + 1 : :]
+    else:
+        return str
