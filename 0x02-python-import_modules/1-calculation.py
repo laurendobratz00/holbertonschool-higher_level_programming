1b9907a9d811f6a6a42f775f5b2e5b49c261f3de
laurendobratz00 <laurendobratz@icloud.com>
2020-12-10 18:38:54 +0000

    if

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-from calculator_1 import add, sub, mul, div
-a = 10
-b = 5
-print("{} + {} = {}".format(a, b, (a + b)))
-print("{} - {} = {}".format(a, b, (a - b)))
-print("{} * {} = {}".format(a, b, (a * b)))
-print("{} / {} = {}".format(a, b, int(a / b)))
+if __name__ == '__main__':
+    from calculator_1 import add, sub, mul, div
+    a = 10
+    b = 5
+    print("{} + {} = {}".format(a, b, (a + b)))
+    print("{} - {} = {}".format(a, b, (a - b)))
+    print("{} * {} = {}".format(a, b, (a * b)))
+    print("{} / {} = {}".format(a, b, int(a / b)))
