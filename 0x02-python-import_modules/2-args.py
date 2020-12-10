78a52b0a21bb3f4691c388a10eb874b79247ac81
laurendobratz00 <laurendobratz@icloud.com>
2020-12-10 23:32:54 +0000

    changed format

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,11 @@
 #!/usr/bin/python3
 if __name__ == '__main__':
     import sys
-    if len(sys.argv) < 1:
+    if (len(sys.argv) - 1) == 0:
         print("0 arguments.")
-    else:
-        print("{} arguments".format((len(sys.argv) - 1)))
+    if (len(sys.argv) - 1) == 1:
+        print("{:d} argument:".format(1))
+    if (len(sys.argv) - 1) > 1:
+        print("{} arguments:".format((len(sys.argv) - 1)))
     for i in range(1, len(sys.argv)):
-        if len(sys.argv[i]) == 1:
-            print("1 argument:")
-            print("{}: {}".format(i, sys.argv[i]))
-        else:
-            print("{}: {}".format(i, sys.argv[i]))
+        print("{}: {}".format(i, sys.argv[i]))
