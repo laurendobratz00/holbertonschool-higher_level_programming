f21a804fcf753e4c0deb8c0044d8d9fa3abee24c
laurendobratz00 <laurendobratz@icloud.com>
2020-12-15 23:31:45 +0000

    add second list

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,6 +3,6 @@ def new_in_list(my_list, idx, element):
     if not my_list:
         return
     my_list2 = my_list[:]
-    if idx >= 0 or idx < len(my_list):
+    if idx >= 0 and idx < len(my_list2):
         my_list2[idx] = element
         return (my_list2)
