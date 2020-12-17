1a0c31a02243844693af7f771b7e75b50d34a43a
laurendobratz00 <laurendobratz@icloud.com>
2020-12-17 22:52:48 +0000

    take away empty list statement

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if not my_list:
-        return
     my_list2 = my_list.copy()
     my_list2 = [x if x != search else replace for x in my_list]
     return my_list2
