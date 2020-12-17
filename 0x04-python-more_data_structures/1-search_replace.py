4c56aa4ef342091ff02db1c7d893ded1a0d2a454
laurendobratz00 <laurendobratz@icloud.com>
2020-12-17 22:43:44 +0000

    if not my_list, return

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
-    if my_list:
-        my_list2 = my_list.copy()
-        my_list2 = [x if x != search else replace for x in my_list]
+    if not my_list:
+        return
+    my_list2 = my_list.copy()
+    my_list2 = [x if x != search else replace for x in my_list]
     return my_list2
