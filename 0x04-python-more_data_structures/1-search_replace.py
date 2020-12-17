331f48683d5c841c06e3a8f5b85d4901c1f16eb8
laurendobratz00 <laurendobratz@icloud.com>
2020-12-17 22:34:50 +0000

    search and replace

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -2,5 +2,5 @@
 def search_replace(my_list, search, replace):
     if my_list:
         my_list2 = my_list[:]
-        my_list2 = [x if x != 2 else 89 for x in my_list]
-        return my_list2
+        my_list2 = [x if x != search else replace for x in my_list]
+    return my_list2
