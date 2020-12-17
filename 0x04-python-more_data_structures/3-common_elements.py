27583f5dae1cf31c12fc0ea6d2dfb932730c8185
laurendobratz00 <laurendobratz@icloud.com>
2020-12-17 23:34:52 +0000

    changed format

--- 0x04-python-more_data_structures/3-common_elements.py
+++ 0x04-python-more_data_structures/3-common_elements.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
 def common_elements(set_1, set_2):
-    a = set(set_1)
-    b = set(set_2)
-    if len(a.intersection(b)) > 0:
-        return (a.intersection(b))
+    c_set = list(set(set_1).intersection(set_2))
+    return c_set
