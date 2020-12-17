b1e0810bc89845e4ad75f2f454c8405cbae5b635
laurendobratz00 <laurendobratz@icloud.com>
2020-12-17 22:38:58 +0000

    copy

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     if my_list:
-        my_list2 = my_list[:]
+        my_list2 = my_list.copy()
         my_list2 = [x if x != search else replace for x in my_list]
-        return my_list2
+    return my_list2
