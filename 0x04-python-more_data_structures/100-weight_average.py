1dc8d7993064e65134ff5051885789b20b60ab2e
laurendobratz00 <laurendobratz@icloud.com>
2020-12-18 20:22:02 +0000

    chmod

--- 0x04-python-more_data_structures/100-weight_average.py
+++ 0x04-python-more_data_structures/100-weight_average.py
@@ -2,4 +2,6 @@
 def weight_average(my_list=[]):
     if not my_list:
         return 0
-    sum(my_list[i] * my_list[i] for i in range(len(my_list))) / sum(my_list)
+    Sum = sum(i[1] for i in my_list)
+    total = sum(i[0] * i[1] for i in my_list)
+    return total / Sum
