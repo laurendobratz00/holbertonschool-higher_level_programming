c622860031e3514aa3cdea8e4a0826b3018689f6
laurendobratz00 <laurendobratz@icloud.com>
2021-01-05 21:58:20 +0000

    documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,12 @@
 #!/usr/bin/python3
+""" This is a square defined with a private instance attribute size
+"""
+
+
 class Square:
+    """ A square defined by private instance attribute and insta    ntiation
+    """
     name = "Square"
+
     def __init__(self, size):
         self.__size = size
