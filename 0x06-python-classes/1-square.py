f7f37fa0671a5de7055bb741b9409a78d5cc5edd
laurendobratz00 <laurendobratz@icloud.com>
2021-01-05 20:41:27 +0000

    add file

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,8 +1,5 @@
 #!/usr/bin/python3
 class Square:
     name = "Square"
-    size = 0
     def __init__(self, size):
-        self.size = size
-    def get_area(self):
-        return size * size
+        self.__size = size
