f885ff21dcef62719f010fec6e8656ee1a191d9b
laurendobratz00 <laurendobratz@icloud.com>
2021-01-06 22:31:13 +0000

    add file

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" This module demonstrates a square """
+
+
+class Square:
+    """ This initializes vars """
+
+    def __init__(self, size=0):
+        """ Initialize the square.
+        Args:
+            size(int): Private attribute for size of square.
+        Return:
+            None
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ getter for the method """
+        return(self.__size)
+
+    @size.setter
+    def size(self, value):
+        """ setter for the method """
+        if type(value) is not int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ multiplying height and width """
+        return self.__size * self.__size
+
+    def my_print(self):
+        """ printing out the stdout of the square """
+        if self.__size is 0:
+            print()
+        for x in range(0, self.__size):
+            print("{}".format("#" * self.__size))
