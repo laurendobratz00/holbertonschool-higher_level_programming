4c4b62ccdc8838caffc340f49a105a2e2f286618
laurendobratz00 <laurendobratz@icloud.com>
2021-01-06 22:20:45 +0000

    add file

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -6,16 +6,29 @@ class Square:
     """ This initializes vars """
 
     def __init__(self, size=0):
+        """ Initialize the square.
+        Args:
+            size(int): Private attribute for size of square.
+        Return:
+            None
+        """
         self.__size = size
 
     @property
     def size(self):
+        """ getter for the method """
         return(self.__size)
 
     @size.setter
-    def size(self, val):
-
-        if type(val) is not int:
+    def size(self, value):
+        """ setter for the method """
+        if type(value) is not int:
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """ multiplying height and width """
+        return self.__size * self.__size
