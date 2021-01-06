8fd1a12aa889303436e1789ab257f14d81cf9a9c
laurendobratz00 <laurendobratz@icloud.com>
2021-01-06 17:29:44 +0000

    add file

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" This module demonstrates a square """
+
+
+class Square:
+    """ This initializes vars """
+
+    def __init__(self, size=0):
+        self.__size = size
+
+    @property
+    def size(self):
+        return(self.__size)
+
+    @size.setter
+    def size(self, val):
+
+        if type(val) is not int:
+            raise TypeError("size must be an integer")
