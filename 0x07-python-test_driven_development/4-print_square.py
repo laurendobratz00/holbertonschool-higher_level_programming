576e9fcf0702272b6457516d5af8390180e0f92c
laurendobratz00 <laurendobratz@icloud.com>
2021-01-09 16:53:03 +0000

    add file

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def text_indentation(text):
+
+    if text is "." and if text is "?" and if text is ":":
+        print()
+        print()
+    else:
+        print("{}".format(text))
