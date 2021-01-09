6915d3c1ca2213b9b599b946566aaa9832c523a7
laurendobratz00 <laurendobratz@icloud.com>
2021-01-09 16:55:15 +0000

    add changes

--- 0x07-python-test_driven_development/2-matrix_divided.py
+++ 0x07-python-test_driven_development/2-matrix_divided.py
@@ -4,12 +4,20 @@
 
 def matrix_divided(matrix, div):
     """ this is a matrix divided by div """
+    len1 = len(matrix[0])
 
-    if isinstance(div, (int, float)) == False:
+    for x, y in enumerate(matrix):
+        if len(y) != len1:
+            raise TypeError("Each row of the matrix must have the same size")
+
+#    if not isinstance(div, (int, float)):
+#        raise TypeError("div must be a number")
+
+    if type(div) is not int and type(div) is not float:
         raise TypeError("div must be a number")
 
     if div is 0:
         raise ZeroDivisionError("division by zero")
 
     if matrix:
-        return [list(map(lambda x: (x / div), x)) for x in matrix]
+        return [list(map(lambda x: (round(x / div, 2)), x)) for x in matrix]
