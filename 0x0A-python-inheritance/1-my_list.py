3e6cd5e4e705e882037a22fb46f036bdb6abb54d
laurendobratz00 <laurendobratz@icloud.com>
2021-01-19 22:03:42 +0000

    add file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" Class MyList that inherits from list
+"""
+
+class MyList(list):
+    """ A class MyList that prints a sorted list
+    """
+
+    def print_sorted(self):
+        """ This method has a public instance and prints a sorted list
+        """
+
+        print(sorted(self))
