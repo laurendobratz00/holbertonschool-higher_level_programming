bcfb0a844285b529073a7bc8f5fc7495a9f95ae4
laurendobratz00 <laurendobratz@icloud.com>
2021-01-20 16:36:45 +0000

    add file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" a function that writes a string to a text file """
+
+
+def write_file(filename="", text=""):
+    """ returns the number of characters written """
+
+    with open("my_first_file.txt", encoding='utf-8') as filename:
+        text = (filename.read())
+        count = len(text)
+        return count
