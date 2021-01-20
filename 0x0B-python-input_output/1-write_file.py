66d15c6a92687bc1632a3fe8b1eb7bc451ec132f
laurendobratz00 <laurendobratz@icloud.com>
2021-01-20 23:28:36 +0000

    add changes

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,7 +5,7 @@
 def write_file(filename="", text=""):
     """ returns the number of characters written """
 
-    with open("my_first_file.txt", encoding='utf-8') as filename:
+    with open(filename, encoding='utf-8') as filename:
         text = (filename.read())
         count = len(text)
         return count
