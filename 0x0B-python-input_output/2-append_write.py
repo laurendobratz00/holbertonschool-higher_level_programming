df3d092b89811feae9bdb7301df6aae16be623ab
laurendobratz00 <laurendobratz@icloud.com>
2021-01-20 16:46:28 +0000

    add file

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" funct that appends string at end of a txt file, returns # of chars added """
+
+
+def append_write(filename="", text=""):
+    """ returns # of characters added"""
+
+    with open("file_append.txt", mode='a', encoding='utf-8') as filename:
+        filename.write("Holberton School is so cool!")
+        count = len(text)
+        return count
