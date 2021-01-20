0a8bb50853a9f075ea9e4aa9612c273aca989baa
laurendobratz00 <laurendobratz@icloud.com>
2021-01-20 22:29:40 +0000

    add file

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" a function that returns an object represented by a string """
+
+
+import json
+
+def from_json_string(my_str):
+    """ returns an object represented by a string """
+    return (json.loads(my_str))
