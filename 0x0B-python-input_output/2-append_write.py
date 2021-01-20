dcebbc5b6344ae9532f7cf6efe930839f3b1473a
laurendobratz00 <laurendobratz@icloud.com>
2021-01-20 23:29:31 +0000

    add changes

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,7 +5,7 @@
 def append_write(filename="", text=""):
     """ returns # of characters added"""
 
-    with open("file_append.txt", mode='a', encoding='utf-8') as filename:
+    with open(filename, mode='a', encoding='utf-8') as filename:
         filename.write("Holberton School is so cool!\n")
         count = len(text)
         return count
