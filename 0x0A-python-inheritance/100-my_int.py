5372adb58c9fa620bd1725f1c0521b9fb2257f7d
laurendobratz00 <laurendobratz@icloud.com>
2021-01-21 17:02:23 +0000

    add changes

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -2,13 +2,13 @@
 """ defines rebel int class """
 
 
-class MyInt(int):
+class MyInt(int)
 """ class myint """
 
-def __eq__(self, other):
-    """ returns opposites """
-    return super().__ne__(other)
+    def __eq__(self, other):
+        """ returns opposites """
+        return super().__ne__(other)
 
-def __ne__(self, other):
-    """ returns opposites """
-    return super().__eq__(other)
+    def __ne__(self, other):
+        """ returns opposites """
+        return super().__eq__(other)
