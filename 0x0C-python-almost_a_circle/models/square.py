becb8933b0f47536b5ec8ca7cbb3cc17c92a2825
laurendobratz00 <laurendobratz@icloud.com>
2021-01-25 17:54:19 +0000

    add file

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -2,14 +2,27 @@
 """ class Square that inherits from Rectangle """
 
 
-Rectangle = __import__('rectangle.py').Rectangle
+from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
     """ class square """
     def __init__(self, size, x=0, y=0, id=None):
-        super().__init__(id)
-        super().__init__(x)
-        super().__init__(y)
-        super().__init__(width) = size
-        super().__init__(height) = size
+        """ instantiation """
+        super().__init__(size, size, x, y, id)
+
+    @property
+    def size(self):
+        """ getter for the method """
+        return (self.width)
+
+    @size.setter
+    def size(self, value):
+        """ setting the width """
+        self.width = value
+        self.height = value
+
+    def __str__(self):
+        """ str method """
+        return ("[Rectangle] ({}) {}/{} - {}".format(self.id, self.x, self.y,
+                self.width))
