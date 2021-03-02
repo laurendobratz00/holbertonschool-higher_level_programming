79848e5ffcc2fc870a91a4bd7aadb8afec4e4fdf
laurendobratz00 <laurendobratz@icloud.com>
2021-03-02 00:26:45 +0000

    add file

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,3 @@
+-- a script that lists the number of records with the same score in second_table
+-- display
+SELECT score, COUNT(score) AS number FROM second_table GROUP BY score ORDER BY score DESC;
