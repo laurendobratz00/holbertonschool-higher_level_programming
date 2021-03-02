b9ad2a3323e821e683739174ff3f629784e81c7c
laurendobratz00 <laurendobratz@icloud.com>
2021-03-02 01:50:01 +0000

    add changes

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,3 +1,3 @@
 -- a script that converts hbtn_0c_0 database to UTF8
 -- Alter
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8 COLLATE utf8mb4_unicode_ci;
