07411fe545f76cf513fcdd69b863a5fd2770e8a2
laurendobratz00 <laurendobratz@icloud.com>
2021-03-02 22:17:51 +0000

    add if not exists

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -1,5 +1,5 @@
 -- a script that creates the database hbtn_0d_usa and the table states
 -- CREATE
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE states (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
+CREATE TABLE IF NOT EXISTS states (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
 name VARCHAR(256) NOT NULL);
