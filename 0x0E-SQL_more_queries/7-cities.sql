923c44aff437ae34a1aeec239b1f0345c28903b7
laurendobratz00 <laurendobratz@icloud.com>
2021-03-03 16:14:45 +0000

    add changes

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,6 +1,9 @@
 -- a script that creates the database hbtn_0d_usa and the table cities
 -- CREATE
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL UNIQUE AUTO_INCREMENT PRIMARY KEY,
-state_id INT NOT NULL FOREIGN KEY REFERENCES id.states(INT),
+CREATE TABLE IF NOT EXISTS hbtn_0d_usa.cities (id INT NOT NULL AUTO_INCREMENT,
+UNIQUE(id),
+PRIMARY KEY(id),
+state_id INT NOT NULL,
+FOREIGN KEY(state_id) REFERENCES hbtn_0d_usa.states(id),
 name VARCHAR(256) NOT NULL);
