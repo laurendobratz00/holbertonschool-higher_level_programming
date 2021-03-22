872e1b30a028e60441a633ee72486cbcbeaf5da7
laurendobratz00 <laurendobratz@icloud.com>
2021-03-22 20:48:14 +0000

    Add file

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+// a script that prints a message depending of the number of arguments passed
+const myArgs = process.argv.slice(2);
+
+if (myArgs.length === 0) {
+    console.log('No argument');
+} else if (myArgs.length === 1) {
+    console.log('Argument found');
+} else {
+console.log('Arguments found');
+}
