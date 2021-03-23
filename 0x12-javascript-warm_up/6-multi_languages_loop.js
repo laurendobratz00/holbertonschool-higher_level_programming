20789ad6440653769281bf83a7abb8b1101cb2ad
laurendobratz00 <laurendobratz@icloud.com>
2021-03-23 01:44:26 +0000

    add file

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+// a script that prints 3 lines
+const myArray = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+
+let i = 0;
+
+for (; i < myArray.length; i++) {
+  console.log(myArray[i]);
+}
