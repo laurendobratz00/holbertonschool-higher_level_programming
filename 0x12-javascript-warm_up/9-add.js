bcee930f5a6d36e2b63cd8e722cc700451ae1e4a
laurendobratz00 <laurendobratz@icloud.com>
2021-03-23 01:15:28 +0000

    semistandard

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -2,7 +2,7 @@
 // a script that prints the addition of 2 integers
 const myArgs = process.argv.slice(2);
 
-function add(a, b) {
+function add (a, b) {
   console.log(parseInt(myArgs[0]) + parseInt(myArgs[1]));
 }
 
