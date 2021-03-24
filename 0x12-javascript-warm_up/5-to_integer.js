30e3cfaac332ae6649f64c4362c1276fcc61eb04
laurendobratz00 <laurendobratz@icloud.com>
2021-03-24 18:37:37 +0000

    added && statement

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
 const myArgs = process.argv.slice(2);
 
-if (myArgs[0]) {
-  console.log('My number: ' + myArgs[0]);
+if (myArgs[0] && Number(myArgs[0])) {
+  console.log('My number: ' + Number(myArgs[0]));
 } else {
   console.log('Not a number');
 }
