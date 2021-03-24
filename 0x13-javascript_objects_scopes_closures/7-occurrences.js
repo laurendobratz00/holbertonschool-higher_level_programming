7ff3779012370d7bfe7b07439a21756cf12eeec3
laurendobratz00 <laurendobratz@icloud.com>
2021-03-24 00:15:54 +0000

    add file

---
+++ 0x13-javascript_objects_scopes_closures/7-occurrences.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+exports.nbOccurences = function (list, searchElement) {
+  const count = list.reduce(function (n, val) {
+    return n + (val === searchElement);
+  }, 0);
+  return (count);
+};
