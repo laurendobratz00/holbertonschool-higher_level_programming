3ef5c3d57ce2dc4ea89ea34cdf4ae8381a442335
laurendobratz00 <laurendobratz@icloud.com>
2021-03-27 18:05:02 +0000

    pep8

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
+import MySQLdb
+from sys import argv
+
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
