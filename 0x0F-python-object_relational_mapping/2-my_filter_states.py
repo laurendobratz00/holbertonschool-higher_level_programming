ed5fdd11baad705b060555ddf2f11220160c1a5b
laurendobratz00 <laurendobratz@icloud.com>
2021-03-27 18:03:36 +0000

    add pep8

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+""" a script that takes in an arg & displays all values in the states table """
+import MySQLdb
+from sys import argv
+
+
 if __name__ == "__main__":
-    import MySQLdb
-    from sys import argv
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
