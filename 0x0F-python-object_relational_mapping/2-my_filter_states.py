a54882e16dbe9c18b491268a0323dd2702183e84
laurendobratz00 <laurendobratz@icloud.com>
2021-03-28 00:40:11 +0000

    add name=

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE '{}'
+    cur.execute("SELECT * FROM states WHERE name LIKE name='{}'
                 COLLATE latin1_general_cs ORDER BY id ASC".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
