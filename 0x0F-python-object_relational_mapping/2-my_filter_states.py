c8064b1523011aafa963a85990b349aba335de39
laurendobratz00 <laurendobratz@icloud.com>
2021-03-28 00:52:55 +0000

    take out charset

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,7 @@ if __name__ == "__main__":
     from sys import argv
     import MySQLdb
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                           passwd=argv[2], db=argv[3], charset="utf8")
+                           passwd=argv[2], db=argv[3])
     cur = conn.cursor()
     cur.execute("""SELECT * FROM states WHERE name='{}' COLLATE
                 latin1_general_cs ORDER BY states.id ASC""".format(argv[4]))
