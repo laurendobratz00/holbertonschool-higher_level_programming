211261ef0e3f9b42de673a637d6a482f4c0dfd67
laurendobratz00 <laurendobratz@icloud.com>
2021-03-28 01:33:48 +0000

    add triple quotes

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3])
     cur = conn.cursor()
-    cur.execute("SELECT cities.id, cities.name, states.name FROM states INNER
-                JOIN cities ON cities.state_id=states.id ORDER BY id ASC")
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM cities INNER
+                JOIN states ON cities.state_id=states.id ORDER BY id ASC""")
     query_rows = cur.fetchall()
     for row in query_rows:
         print(row)
