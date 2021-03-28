f9c5edc13246026781a1e2144c73c23512f02b51
laurendobratz00 <laurendobratz@icloud.com>
2021-03-28 00:47:34 +0000

    changes made

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 """ a script that takes in an arg & displays all values in the states table """
+
 if __name__ == "__main__":
     import MySQLdb
     from sys import argv
     conn = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
                            passwd=argv[2], db=argv[3], charset="utf8")
     cur = conn.cursor()
-    cur.execute("""SELECT * FROM states WHERE name LIKE name='{}' COLLATE
+    cur.execute("""SELECT * FROM states WHERE name='{}' COLLATE
                 latin1_general_cs ORDER BY states.id ASC""".format(argv[4]))
     query_rows = cur.fetchall()
     for row in query_rows:
