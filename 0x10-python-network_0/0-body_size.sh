eeed806992ede3012d72c116aab06d15abddbbbd
laurendobratz00 <laurendobratz@icloud.com>
2021-04-08 22:30:30 +0000

    add

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # a Bash script that sends a request to that URL,and displays the size of body
-curl -sI $URL | grep -i Content-Length | awk '{print $2}'
+curl -sI $1 | grep -i Content-Length | awk '{print $2}'
