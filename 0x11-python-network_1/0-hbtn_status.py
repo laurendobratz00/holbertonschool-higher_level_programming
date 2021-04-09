f91ab239dbd9ca6390fba058d962206cf2d8ca11
laurendobratz00 <laurendobratz@icloud.com>
2021-04-09 20:07:16 +0000

    add print statements

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,5 +1,13 @@
 #!/usr/bin/python3
 """ a python script that fetches https://intranet.hbtn.io/status """
 import urllib.request
+
+
 with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
     html = response.read()
+    utf8 = html.decode('utf-8')
+
+    print("Body response:")
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(utf8))
