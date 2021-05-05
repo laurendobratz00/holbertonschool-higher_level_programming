312f419bdd739d5dc527dd70fa00669f7c86b81b
laurendobratz00 <laurendobratz@icloud.com>
2021-05-05 19:14:39 +0000

    add file

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+const myargs = process.argv.slice(2);
+
+fs.writeFile(myargs[0], myargs[1], (err, data) => {
+  if (err) {
+    console.log(err);
+  }
+});
