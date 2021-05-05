d8ad14ce49cfffe0dfcaa8cb2ab72d13b9912cba
laurendobratz00 <laurendobratz@icloud.com>
2021-05-05 18:51:13 +0000

    semistandard

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -5,9 +5,9 @@ const process = require('process');
 const myargs = process.argv.slice(2);
 
 fs.readFile(myargs[0], (err, data) => {
-    if (err) {
-	console.log(err);
-    } else {
-	console.log(data.toString());
-    }
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(data.toString());
+  }
 });
