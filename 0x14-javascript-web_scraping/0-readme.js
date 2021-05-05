7adb61312c2a810a35be654c143c88beef102c3c
laurendobratz00 <laurendobratz@icloud.com>
2021-05-05 18:48:39 +0000

    add file

---
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+const fs = require('fs');
+const process = require('process');
+
+const myargs = process.argv.slice(2);
+
+fs.readFile(myargs[0], (err, data) => {
+    if (err) {
+	console.log(err);
+    } else {
+	console.log(data.toString());
+    }
+});
