8493974277a85382672de556ff8fcf3cfccfade9
laurendobratz00 <laurendobratz@icloud.com>
2021-05-11 19:52:25 +0000

    changed format

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -4,15 +4,14 @@ const request = require('request');
 request.get('https://swapi-api.hbtn.io/api/films/', function (err, res, body) {
   if (err) {
     console.log(err);
-  } else {
-    let o = 0;
-    for (const film of JSON.parse(body).results) {
-      for (const character of film.characters) {
-        if (character.includes('18')) {
-          o = o + 1;
-        }
+  }
+  let o = 0;
+  for (const film of JSON.parse(body).results) {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        o = o + 1;
       }
     }
-    console.log(o);
   }
+  console.log(o);
 });
