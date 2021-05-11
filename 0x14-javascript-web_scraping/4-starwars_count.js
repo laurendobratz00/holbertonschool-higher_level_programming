fe5ce4ae8cca433e19bd014f66008b659622218a
laurendobratz00 <laurendobratz@icloud.com>
2021-05-11 19:32:17 +0000

    add file

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const request = require('request');
+
+request.get('https://swapi-api.hbtn.io/api/films/', function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
+    let o = 0;
+    for (const film of JSON.parse(body).results) {
+      for (const character of film.characters) {
+        if (character.includes('18')) {
+          o = o + 1;
+        }
+      }
+    }
+    console.log(o);
+  }
+});
