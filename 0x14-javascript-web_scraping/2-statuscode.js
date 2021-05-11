5ae40d2816c97817a917155eb5a314d487dd79dd
laurendobratz00 <laurendobratz@icloud.com>
2021-05-11 18:41:10 +0000

    semistandard

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -5,6 +5,10 @@ const request = require('request');
 
 const myargs = process.argv[2];
 
-request(myargs, function(err, res, body) {
+request(myargs, function (err, res, body) {
+  if (err) {
+    console.log(err);
+  } else {
     console.log('code:', res.statusCode);
+  }
 });
